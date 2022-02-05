17517c6abce0266444130d2a1e25f9b8d24dabef
Marko <marko@a.com>
2022-02-05 11:47:57 +0100

    app.js to /api/index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
-const { notFound, errorHandler } = require("./middleware/errorHandlers.js");
+const { notFound, errorHandler } = require("../middleware/errorHandlers.js");
 const res = require("express/lib/response");
 
 const app = express();
@@ -13,11 +13,16 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("combined"));
 
+app.use((req, res, next) => {
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  next();
+});
+
 // Home route
 app.get("/", (req, res) => res.json({ message: "Greetings from Mordax!" }));
 
 // Routes
-app.use("/api/1", require("./api/router.js"));
+app.use("/api/1", require("./router.js"));
 
 // Error handlers
 app.use(notFound);
