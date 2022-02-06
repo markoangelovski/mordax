5e239323c7e85096bcdfe4ae34c3ca34886cced1
Marko <marko@a.com>
2022-02-06 12:34:52 +0100

    Heroku proxy test 6

--- api/sc/sc.routes.js
+++ api/sc/sc.routes.js
@@ -266,6 +266,10 @@ router.get("/sc-data-product", async (req, res, next) => {
   // Vercel app does not fetch retailer links so Heroku app needs to be used
   if (req.headers["x-vercel-forwarded-for"]) {
     const query = new URLSearchParams(req.query);
+    console.log(
+      "path: ",
+      require("../../config").hostHeroku + "?" + query.toString()
+    );
     return axios(require("../../config").hostHeroku + "?" + query.toString())
       .then(response => res.json(response.data))
       .catch(err => next(err));
